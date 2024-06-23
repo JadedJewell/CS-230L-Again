e5a0323fdccbf880bce93cc595081d5a6a7c208a
“Sigmoid” <saj0025@mix.wvu.edu>
2024-06-23 15:51:37 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 702</h2>
+      <p>WVU ID: 800313162</p>
+      <p>Hi I am Shawn</p>
+    </div>
+  );
+}
+
+export default App;
