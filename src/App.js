e41fc1f09564bf50f7f7b76ec3c8f09ae74a2a7f
“JadedJewell” <saj0025@mix.wvu.edu>
2024-06-30 13:47:53 -0400

    Changes for final Lab

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import Card from './Componets/Card'
+import Navbar from './Componets/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 702</h2>
       <p>WVU ID: 800313162</p>
       <p>Hi I am Shawn</p>
+      <div class="card-group">
+        <Card title="Card 01" />
+        <Card title="Card 02" />
+        <Card title="Card 03" />
+      </div>
     </div>
   );
 }
